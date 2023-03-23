6a454a452ca976abb610eea0b01dae66c79a26a1
david <davidlab44@gmail.com>
2023-03-23 00:45:38 -0400

    GetRecipesUseCase test passed

---
+++ app/src/test/java/com/david/epay/domain/model/GetRecipesUseCaseTest.kt
@@ -0,0 +1,34 @@
+package com.david.epay.domain.model
+
+import com.david.epay.data.RecipeRepository
+import com.david.epay.domain.GetRecipesUseCase
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+
+class GetRecipesUseCaseTest{
+
+    @RelaxedMockK
+    private lateinit var recipeRepository: RecipeRepository
+    lateinit var getRecipesUseCase: GetRecipesUseCase
+
+    @Before
+    fun onBefore(){
+        MockKAnnotations.init(this)
+        getRecipesUseCase = GetRecipesUseCase(recipeRepository)
+    }
+
+    @Test
+    fun `when the api does not return anything then get values from database`()= runBlocking {
+        //given
+        coEvery { recipeRepository.getAllRecipesFromApi() } returns emptyList()
+        //when
+        getRecipesUseCase()
+        //then
+        coEvery (){ recipeRepository.getAllRecipesFromDatabase() }
+    }
+}
